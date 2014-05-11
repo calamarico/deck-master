53b2cd73d92078797880052f81bc4c6179caba77
Kalamarico <daniel.hernandez.zafra@gmail.com>
2014-05-11 14:20:55 +0200

    Create development configuration using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,55 @@ module.exports = function(grunt) {
       grunt: 'Gruntfile.js',
       tmp: '<%= source.build %>/tmp',
       publish: '<%= source.build %>/publish/'
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: 'app'
+        }
+      }
+    },
+    jshint: {
+      files: ['gruntfile.js', 'app/js/*.js', 'test/**/*.spec.js']
+    },
+    lesslint: {
+      src: ['app/styles/*.less']
+    },
+    less: {
+      dev: {
+        options: {
+          dumpLineNumbers: 'comments',
+          //cleancss: true,
+          imports: {
+            reference: [
+              'app/styles/variables.less',
+              'app/styles/bootstrap/variables.less',
+              'app/styles/bootstrap/mixins.less']
+          }
+        },
+        files: [{
+          expand: true,
+          cwd: 'app/styles/',
+          src: ['style.less', 'bootstrap/bootstrap.less'],
+          dest: 'app/styles/css/',
+          ext: '.css'
+        }]
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'less']
     }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-lesslint');
+
+  grunt.registerTask('linter', ['jshint', 'lesslint']);
+  grunt.registerTask('default', ['jshint', 'lesslint', 'less', 'connect',
+    'watch']);
 };
---
+++ app/js/app.js
@@ -0,0 +1,2 @@
+/* App.js */
+angular.module('deckMasterApp', []);
