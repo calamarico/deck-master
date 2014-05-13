cb48c1458d57d2c36b1ce143c1c8016869541925
Kalamarico <daniel.hernandez.zafra@gmail.com>
2014-05-13 21:02:17 +0200

    Set first and temporal grid layout, issue/4

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,12 @@ module.exports = function(grunt) {
       files: ['gruntfile.js', 'app/js/*.js', 'test/**/*.spec.js']
     },
     lesslint: {
-      src: ['app/styles/*.less']
+      src: ['app/styles/*.less'],
+      options: {
+        csslint: {
+          csslintrc: '.csslintrc'
+        }
+      }
     },
     less: {
       dev: {
