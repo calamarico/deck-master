173c30bdda39a8d90638be8c311def39fa3f5b5d
Kalamarico <daniel.hernandez.zafra@gmail.com>
2014-05-15 21:09:05 +0200

    Add directive and controller of sets, issue/6

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,1 +1,33 @@
-/* Controllers.js */
+/**
+ * Sets controller.
+ */
+angular.module('deckMasterApp')
+.controller('setsController', ['$scope', 'mtgdbApi',
+  function($scope, mtgdb) {
+    function _getSetsInfo() {
+      mtgdb.getAllSets().then(function(response) {
+        var i;
+
+        for (i = 0; i < response.data.length; i++) {
+          var _set = {},
+            _date = new Date(response.data[i].releasedAt),
+            _fullYear = _date.getFullYear();
+
+          if (typeof $scope.sets[_fullYear] === 'undefined') {
+            $scope.sets[_fullYear] = [];
+            $scope.sets[_fullYear].fullYear = _fullYear;
+          }
+          angular.extend(_set, response.data[i]);
+          $scope.sets[_fullYear].push(response.data[i]);
+        }
+        console.log($scope.sets);
+      }, function(error) {
+        // TODO: error control
+      });
+    }
+
+    $scope.sets = {};
+
+    _getSetsInfo();
+  }
+]);
